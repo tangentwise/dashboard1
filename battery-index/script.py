341700ffbb3be64a9306369d9054917fb6ba85ea
tangentwise <frankwisereading@gmail.com>
2026-01-28 22:56:36 -0500

    Update script.py

--- battery-index/script.py
+++ battery-index/script.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import json
 
 # New "Hard Asset" tickers: LITP (Miners), ION (All Metals), LIT (Value Chain), ^DJC (Commodities)
-tickers = ["LIT", "LITP", "ION", "^DJC"]
+tickers = ["LIT", "LITP", "ION", "GSG"]
 df = yf.download(tickers, period="2y", interval="1mo")['Adj Close'].dropna()
 
 data_dict = {t: [{"date": d.strftime('%Y-%m'), "price": round(v, 2)} 
