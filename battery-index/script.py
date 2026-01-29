a3c8eaaba8ff32b2f2a1ca24356554287d46290e
tangentwise <frankwisereading@gmail.com>
2026-01-28 22:33:33 -0500

    Add script to download and save hard asset prices
    This script downloads adjusted close prices for specified hard asset tickers over a two-year period and saves the data to a JSON file.

---
+++ battery-index/script.py
@@ -0,0 +1,12 @@
+import yfinance as yf
+import json
+
+# New "Hard Asset" tickers: LITP (Miners), ION (All Metals), LIT (Value Chain), ^DJC (Commodities)
+tickers = ["LIT", "LITP", "ION", "^DJC"]
+df = yf.download(tickers, period="2y", interval="1mo")['Adj Close'].dropna()
+
+data_dict = {t: [{"date": d.strftime('%Y-%m'), "price": round(v, 2)} 
+             for d, v in df[t].items()] for t in tickers}
+
+with open('battery-index/data.json', 'w') as f:
+    json.dump(data_dict, f)
